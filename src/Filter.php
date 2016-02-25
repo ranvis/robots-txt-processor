a8163684a8cbe72d86187ee8871947a2e3321d44
SATO Kentaro <kentaro@ranvis.com>
2016-02-26 04:57:18 +0900

    Filter.

---
+++ src/Filter.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ * @author SATO Kentaro
+ * @license BSD 2-Clause License
+ */
+
+namespace Ranvis\RobotsTxt;
+
+class Filter
+{
+    const NON_GROUP_RULES = '#';
+
+    protected $options;
+    protected $records;
+    protected $targetUserAgents;
+    private $availUserAgents;
+
+    /**
+     * @param $options Parse options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->options = $options + [
+            'maxRecords' => 1000,
+            'maxUserAgents' => 1000,
+            'maxNameLength' => 200,
+            'maxLineLength' => 2000,
+            'maxWildcards' => 10,
+            'escapedWildcard' => true,
+            'supportLws' => false,
+            'pathMemberRegEx' => '/^(?i)(?:Dis)?Allow$/',
+        ];
+    }
+
+    /**
+     * Filter records on parse by User-agents
+     *
+     * @param string|array $userAgents User-agents to keep
+     * @param bool $fallback True to keep fallback '*' record
+     * @return string|null Rules
+     */
+    public function setUserAgents($userAgents, bool $fallback = true)
+    {
+        $userAgents = (array)$userAgents;
+        if ($fallback) {
+            $userAgents[] = '*';
+        }
+        $this->targetUserAgents = array_flip(array_map(function ($userAgent) {
+            return strtolower($userAgent); // locale dependent; who cares?
+        }, $userAgents));
+    }
+
+    /**
+     * Parse robots.txt string
+     *
+     * @param $source robots.txt data
+     */
+    public function parse(string $source)
+    {
+        $this->availUserAgents = $this->options['maxUserAgents'];
+        $records = $this->splitSource($source);
+        $namedRecords = [];
+        foreach ($records as $record) {
+            $spec = $this->getRecordSpec($record);
+            $merge = false;
+            if ($spec['nonGroup']) { // merge non-group records
+                // cf. nongroupline of https://developers.google.com/webmasters/control-crawl-index/docs/robots_txt?hl=en
+                $userAgent = self::NON_GROUP_RULES;
+                if (isset($namedRecords[$userAgent])) {
+                    $namedRecords[$userAgent] .= "\n";
+                } else {
+                    $namedRecords[$userAgent] = '';
+                }
+                $namedRecords[$userAgent] .= $spec['rules'];
+                continue;
+            }
+            foreach ($spec['userAgents'] as $userAgent) {
+                $rules = $spec['rules'];
+                if (!$this->targetUserAgents || isset($this->targetUserAgents[$userAgent])) {
+                    $namedRecords[$userAgent] = $rules;
+                }
+            }
+        }
+        $this->records = $namedRecords;
+    }
+
+    public function getFilteredSource($userAgents = null)
+    {
+        $rules = (string)$this->getRules($userAgents);
+        $it = $this->getRuleIterator($rules);
+        $filteredRules = 'User-agent: *';
+        foreach ($it as $rule) {
+            $line = $rule['key'] . ': ' . $rule['value'];
+            $filteredRules .= "\n$line";
+        }
+        if ($nonGroupRules = $this->getNonGroupRules()) {
+            $filteredRules .= "\n\n$nonGroupRules";
+        }
+        return $filteredRules;
+    }
+
+    public function getRuleIterator(string $rules)
+    {
+        $it = $this->getRecordIterator($rules);
+        $pathMemberRegEx = $this->options['pathMemberRegEx'];
+        $maxWildcards = $this->options['maxWildcards'];
+        $escapedWildcard = $this->options['escapedWildcard'];
+        foreach ($it as $line) {
+            if (!preg_match('/\A(?<key>[^:]+?)[ \t]*:[ \t]*(?<value>.*)/', $line, $match)) {
+                continue;
+            }
+            if (preg_match($pathMemberRegEx, $match['key'])) {
+                $path = $match['value'];
+                if ($escapedWildcard) {
+                    $path = preg_replace('/%2a/i', '*', $path);
+                }
+                if (!preg_match('#\A[/*]#', $path)) {
+                    continue;
+                }
+                $path = preg_replace('/\*+/', '*', $path, -1, $count);
+                if ($count > $maxWildcards) {
+                    continue;
+                }
+                $match['value'] = $path;
+            }
+            yield $match;
+        }
+    }
+
+    /**
+     * Get rules for the first specified User-agents or '*'
+     *
+     * @param string|array|null $userAgents User-agents in order of preference
+     * @return string|null Rules
+     */
+    public function getRules($userAgents = null)
+    {
+        if ($userAgents === null) {
+            $userAgents = $this->targetUserAgents;
+        }
+        $userAgents = (array)$userAgents;
+        $userAgents[] = '*';
+        foreach ($userAgents as $userAgent) {
+            $rules = $this->getRawRules($userAgent);
+            if ($rules !== null) {
+                break;
+            }
+        }
+        return $rules;
+    }
+
+    /**
+     * Get rules for the specified User-agent
+     *
+     * @param $userAgent User-agent
+     * @return string|null Rules
+     */
+    public function getRawRules(string $userAgent)
+    {
+        $userAgent = strtolower($userAgent);
+        return isset($this->records[$userAgent]) ? $this->records[$userAgent] : null;
+    }
+
+    public function getNonGroupRules()
+    {
+        return $this->getRawRules(self::NON_GROUP_RULES);
+    }
+
+    protected function splitSource(string $source)
+    {
+        $maxRecords = $this->options['maxRecords'];
+        if ($this->options['supportLws']) {
+            $source = preg_replace('/\x0d\x0a[ \t]+/s', ' ', $source);
+        }
+        $records = preg_split('/(?:\x0d\x0a?|\x0a){2,}/s', $source, $maxRecords ? ($maxRecords + 1) : -1);
+        if ($maxRecords && count($records) > $maxRecords) {
+            array_pop($records);
+        }
+        return $records;
+    }
+
+    protected function getRecordSpec(string $record)
+    {
+        $maxNameLength = (int)$this->options['maxNameLength'];
+        $userAgents = [];
+        $uaRegEx = '/\AUser-agent[ \t]*:[ \t]*(.{0,' . $maxNameLength . '})/i';
+        $it = $this->getRecordIterator($record);
+        $isNonGroup = true;
+        foreach ($it as $line) {
+            if (!preg_match($uaRegEx, $line, $match)) {
+                $it->send(true);
+                break;
+            }
+            $isNonGroup = false;
+            if ($this->availUserAgents > 0) {
+                $this->availUserAgents--;
+                $userAgents[strtolower($match[1])] = true;
+            }
+        }
+        $record = $it->getReturn();
+        return [
+            'nonGroup' => $isNonGroup,
+            'userAgents' => array_keys($userAgents),
+            'rules' => $record,
+        ];
+    }
+
+    protected function getRecordIterator(string $record)
+    {
+        $record = rtrim($record, "\x0a\x0d");
+        $maxLineLength = $this->options['maxLineLength'];
+        while (strlen($record)) {
+            $lines = preg_split('/(?:\x0d\x0a?|\x0a)+/s', $record, 2);
+            $line = $lines[0];
+            if (strlen($line) <= $maxLineLength) {
+                $line = preg_replace('/[ \t]*#.*$/s', '', $line); // remove comment
+                $line = ltrim($line, " \t"); // remove leading spaces
+                if (strlen($line)) {
+                    $msg = (yield $line);
+                    if ($msg === true) {
+                        break;
+                    }
+                }
+            }
+            $record = count($lines) > 1 ? $lines[1] : '';
+        }
+        return $record;
+    }
+}
---
+++ tests/FilterTest.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * @author SATO Kentaro
+ * @license BSD 2-Clause License
+ */
+
+class ParserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getTestSetUserAgentsData
+     */
+    public function testSetUserAgents($filterArgs, $target, $expected)
+    {
+        $source = "User-agent: a\ndata-a\n\nUser-agent: b\ndata-b\n\nUser-agent: *\nfallback";
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        $parser->setUserAgents(...$filterArgs);
+        $parser->parse($source);
+        $this->assertSame($expected, $parser->getRules($target));
+    }
+
+    public function getTestSetUserAgentsData()
+    {
+        return [
+            [['a'], 'a', 'data-a'],
+            [['a'], 'b', 'fallback'],
+            [['a'], 'c', 'fallback'],
+            [[['a', 'b']], 'a', 'data-a'],
+            [[['a', 'b']], 'b', 'data-b'],
+            [[['a', 'b']], 'c', 'fallback'],
+            [['c'], 'a', 'fallback'],
+            [['c'], 'b', 'fallback'],
+            [['c'], 'c', 'fallback'],
+            [['a', false], 'a', 'data-a'],
+            [['a', false], 'b', null],
+            [['a', false], 'c', null],
+        ];
+    }
+
+    public function testParse()
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        $parser->parse('');
+        $this->assertNull($parser->getRawRules('a'));
+        $this->assertNull($parser->getRawRules('*'));
+    }
+
+    /**
+     * @dataProvider getTestGetFilteredSourceData
+     */
+    public function testGetFilteredSource($ua, $data)
+    {
+        $source = "User-agent: a\nkey:value-a\n\nUser-agent: b\nkey:value-b\n\nUser-agent: *\nkey:fallback\n\nkey:non-group";
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        $parser->parse($source);
+        $this->assertSame($data, $parser->getFilteredSource($ua));
+    }
+
+    public function getTestGetFilteredSourceData()
+    {
+        return [
+            ['a', "User-agent: *\nkey: value-a\n\nkey:non-group"],
+            ['b', "User-agent: *\nkey: value-b\n\nkey:non-group"],
+            ['c', "User-agent: *\nkey: fallback\n\nkey:non-group"],
+        ];
+    }
+
+    public function testGetRuleIterator()
+    {
+        $source = "Name: value#comment\nAllow:***path \t#comment\n#comment\nDisallow:  /path \t\nDisallow: not-this-path\nNo;Colon\nName \t:\t va***lue\t ";
+        $expectedList = [
+            ['key' => "Name", 'value' => "value"],
+            ['key' => "Allow", 'value' => "*path"],
+            ['key' => "Disallow", 'value' => "/path \t"],
+            ['key' => "Name", 'value' => "va***lue\t "],
+        ];
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        foreach ($parser->getRuleIterator($source) as $rule) {
+            $expected = array_shift($expectedList);
+            $this->assertSame($expected['key'], $rule['key']);
+            $this->assertSame($expected['value'], $rule['value']);
+        }
+    }
+
+    public function testGetRules()
+    {
+    }
+
+    public function testGetRawRules()
+    {
+        $source = "User-agent: a\ndata-a\n\nUser-agent: b\ndata-b\n\nUser-agent: *\nfallback\n\nnon-group";
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        $parser->parse($source);
+        $this->assertSame('data-a', $parser->getRawRules('a'));
+        $this->assertSame('data-b', $parser->getRawRules('b'));
+        $this->assertSame(null, $parser->getRawRules('c'));
+        $this->assertSame('fallback', $parser->getRawRules('*'));
+    }
+
+    /**
+     * @dataProvider getTestGetNonGroupRulesData
+     */
+    public function testGetNonGroupRules($source, $expected)
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter();
+        $parser->parse($source);
+        $this->assertSame($expected, $parser->getNonGroupRules());
+    }
+
+    public function getTestGetNonGroupRulesData()
+    {
+        return [
+            [
+                "non-group\n\nUser-agent: a\ndata-a\n\nUser-agent: b\ndata-b\n\nUser-agent: *\nfallback",
+                "non-group"
+            ], [
+                "User-agent: a\ndata-a\n\nUser-agent: b\ndata-b\n\nUser-agent: *\nfallback\n\nnon-group",
+                "non-group"
+            ], [
+                "non-group-1\n\nUser-agent: a\ndata-a\n\nUser-agent: b\ndata-b\n\nUser-agent: *\nfallback\n\nnon-group-2",
+                "non-group-1\nnon-group-2"
+            ],
+        ];
+    }
+
+    public function testOptionMaxRecords()
+    {
+        $source = "User-agent: a\ndata-a\n\nUser-agent: b\nUser-agent: b2\ndata-b\n\nUser-agent: c\ndata-c";
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxRecords' => 1000]);
+        $parser->parse($source);
+        $this->assertSame('data-c', $parser->getRawRules('c'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxRecords' => 3]);
+        $parser->parse($source);
+        $this->assertSame('data-c', $parser->getRawRules('c'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxRecords' => 2]);
+        $parser->parse($source);
+        $this->assertNull($parser->getRawRules('c'));
+        $this->assertSame('data-b', $parser->getRawRules('b'));
+        $this->assertSame('data-b', $parser->getRawRules('b2'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxRecords' => 0]);
+        $this->assertNull($parser->getRawRules('a'));
+        $this->assertNull($parser->getNonGroupRules());
+    }
+
+    public function testOptionMaxUserAgents()
+    {
+        $source = "User-agent: a\nUser-agent: b\nUser-agent: c\ndata";
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxUserAgents' => 1000]);
+        $parser->parse($source);
+        $this->assertSame('data', $parser->getRawRules('c'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxUserAgents' => 3]);
+        $parser->parse($source);
+        $this->assertSame('data', $parser->getRawRules('c'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxUserAgents' => 2]);
+        $parser->parse($source);
+        $this->assertNull($parser->getRawRules('c'));
+        $this->assertSame('data', $parser->getRawRules('b'));
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxUserAgents' => 0]);
+        $this->assertNull($parser->getRawRules('a'));
+        $this->assertNull($parser->getNonGroupRules());
+    }
+
+    /**
+     * @dataProvider getTestOptionMaxNameLengthData
+     */
+    public function testOptionMaxNameLength($max, $uaRules, $nonGroup)
+    {
+        $source = "User-agent: 123\nUser-agent: 12345\nUser-agent: 1234\ndata";
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxNameLength' => $max]);
+        $parser->parse($source);
+        $this->assertSame($uaRules[0], $parser->getRawRules('123'));
+        $this->assertSame($uaRules[1], $parser->getRawRules('1234'));
+        $this->assertSame($uaRules[2], $parser->getRawRules('12345'));
+        $this->assertSame($nonGroup, $parser->getNonGroupRules());
+    }
+
+    public function getTestOptionMaxNameLengthData()
+    {
+        return [
+            [1000, ['data', 'data', 'data'], null],
+            [5, ['data', 'data', 'data'], null],
+            [4, ['data', 'data', null], null],
+            [3, ['data', null, null], null],
+            [2, [null, null, null], null],
+            [0, [null, null, null], null],
+        ];
+    }
+
+    /**
+     * @dataProvider getTestOptionMaxLineLengthData
+     */
+    public function testOptionMaxLineLength($max, $source, $expected)
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxLineLength' => $max]);
+        $parser->parse($source);
+        $this->assertSame($expected, $parser->getRawRules('*'));
+    }
+
+    public function getTestOptionMaxLineLengthData()
+    {
+        return [
+            [1000, "User-agent: *\n123456789012345678\n123456789012345", "123456789012345678\n123456789012345"],
+            [18, "User-agent: *\n123456789012345678\n123456789012345", "123456789012345678\n123456789012345"],
+            [17, "User-agent: *\n123456789012345678\n123456789012345", "123456789012345"],
+            [15, "User-agent: *\n123456789012345678\n123456789012345", "123456789012345"],
+            [14, "User-agent: *\n123456789012345678\n123456789012345", ""],
+            [12, "User-agent: *\n123456789012345678\n123456789012345", null],
+            [0, "User-agent: *\n123456789012345678\n123456789012345", null],
+        ];
+    }
+
+    /**
+     * @dataProvider getTestOptionMaxWildcardsData
+     */
+    public function testOptionMaxWildcards($max, $source, $expected)
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxWildcards' => $max]);
+        $header = "User-agent: *";
+        $parser->parse($header . "\n" . $source);
+        $actual = $parser->getFilteredSource('*');
+        $this->assertSame($header . (strlen($expected) ? "\n" : '') . $expected, $actual);
+    }
+
+    public function getTestOptionMaxWildcardsData()
+    {
+        return [
+            [5, 'Name: *****v****a***l**u*e', 'Name: *****v****a***l**u*e'],
+            [4, 'Name: *****v****a***l**u*e', 'Name: *****v****a***l**u*e'],
+            [1, 'Name: *****v****a***l**u*e', 'Name: *****v****a***l**u*e'],
+            [0, 'Name: *****v****a***l**u*e', 'Name: *****v****a***l**u*e'],
+            [5, 'Allow: *****v****a***l**u*e', 'Allow: *v*a*l*u*e'],
+            [4, "Allow: *****v****a***l**u*e", ''],
+            [4, "Allow: *****v****a***l**ue\nAllow: *****v****a***l**u*e", 'Allow: *v*a*l*ue'],
+        ];
+    }
+
+    /**
+     * @dataProvider getTestOptionEscapedWildcardData
+     */
+    public function testOptionEscapedWildcard($max, $flag, $source, $expected)
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter(['maxWildcards' => $max, 'escapedWildcard' => $flag]);
+        $header = "User-agent: *";
+        $parser->parse($header . "\n" . $source);
+        $actual = $parser->getFilteredSource('*');
+        $this->assertSame($header . (strlen($expected) ? "\n" : '') . $expected, $actual);
+    }
+
+    public function getTestOptionEscapedWildcardData()
+    {
+        return [
+            [3, true, 'Disallow: */%2a%2A/%2a%2A', 'Disallow: */*/*'],
+            [3, true, 'Disallow: */%2a%2A*%2a%2A', 'Disallow: */*'],
+            [2, true, 'Disallow: */%2a%2A/%2a%2A', ''],
+            [2, true, 'Disallow: */%2a%2A*%2a%2A', 'Disallow: */*'],
+            [3, false, 'Disallow: */%2a%2A/%2a%2A', 'Disallow: */%2a%2A/%2a%2A'],
+            [3, false, 'Disallow: */%2a%2A/%2a%2A', 'Disallow: */%2a%2A/%2a%2A'],
+            [2, false, 'Disallow: */%2a%2A/%2a%2A', 'Disallow: */%2a%2A/%2a%2A'],
+            [2, false, 'Disallow: */%2a%2A*%2a%2A', 'Disallow: */%2a%2A*%2a%2A'],
+            [1, false, 'Disallow: */%2a%2A/%2a%2A', 'Disallow: */%2a%2A/%2a%2A'],
+            [0, false, 'Disallow: */%2a%2A/%2a%2A', ''],
+        ];
+    }
+
+    public function testOptionSupportLws()
+    {
+        $parser = new \Ranvis\RobotsTxt\Filter(['supportLws' => false]);
+        $source = "User-agent\n \t: \t\n \ta";
+        $parser->parse($source);
+        $this->assertNull($parser->getRawRules('a'));
+        $this->assertSame($source, $parser->getNonGroupRules());
+        $parser = new \Ranvis\RobotsTxt\Filter(['supportLws' => true]);
+        $parser->parse($source);
+        $this->assertNull($parser->getRawRules('a'));
+        $this->assertSame($source, $parser->getNonGroupRules());
+        $source = str_replace("\n", "\x0d\x0a", $source);
+        $parser = new \Ranvis\RobotsTxt\Filter(['supportLws' => true]);
+        $parser->parse($source);
+        $this->assertSame('', $parser->getRawRules('a'));
+        $this->assertNull($parser->getNonGroupRules());
+    }
+}
